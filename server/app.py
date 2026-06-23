d8abd9eca7d48fe4aee7f6fb2b8cee0a457b5d34
St Michael Egenamba <michaelegenamba@gmail.com>
2026-06-22 23:33:53 -0700

    Pushing to codegrade 1# attempt

--- server/app.py
+++ server/app.py
@@ -17,5 +17,72 @@ class ClearSession(Resource):
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 
+class Signup(Resource):
+
+    def post(self):
+
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+       
+        if user:
+            return {"error": "User already exists"}, 400
+        
+        new_user = User(username=username)
+        new_user.password_hash = password
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        return UserSchema().dump(new_user), 201
+    
+
+class Login(Resource):
+    def post(self):
+
+        data = request.get_json() or {}
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+
+        return {'Error': "Unauthorized"}, 401
+
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return UserSchema().dump(user), 200
+        
+        return {}, 401
+    
+class Logout(Resource):
+    
+    def delete(self):
+        
+        session.clear()
+        return {}, 204
+
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+class ClearSession(Resource):
+
+    def delete(self):
+        session.clear()
+        return {}, 204
